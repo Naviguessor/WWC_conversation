1fc907709f70fc48391b7043c5316fb448654405
Naviguessor <davis.veronica.c@gmail.com>
2014-12-08 19:44:34 -0800

    added Vee's files

---
+++ Python/webscraper.py
@@ -0,0 +1,5 @@
+"""
+This file is to be used as the main driver for the webscraping program.
+Any additional functions created in other modules should be imported
+and used here to streamline the webscraping process.
+"""
